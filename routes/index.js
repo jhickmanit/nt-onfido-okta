c73f9b04e53d0ce1c3c2db8e1874fbc2f5f578f5
Jeff Hickman <jhickman.it@gmail.com>
2020-10-07 15:40:40 -0700

    add sample env

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const configuration = require('../util/config');
 const { getApplicantId } = require('../services/okta');
 const { getSDKToken, startCheck } = require('../services/onfido');
